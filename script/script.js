c6fd4b2140440a1a6c47a1fd7db737cb867f9072
tolkoxa <tolkoxa@ya.ru>
2020-10-06 13:25:17 +0300

    Версия 1.0 (десктопная, планшетная)

--- script/script.js
+++ script/script.js
@@ -6,11 +6,10 @@ let app = new Vue({
 });
 
 class Alpabet {
-    constructor(countAnswer = 0, rightAnswer = 0, letterCount = 1) {
+    constructor(countAnswer = 0, rightAnswer = 0, letterCount = 1, i = 0) {
         this._init();
         this.allData; //массив из json–файла
         this.letterArr = []; //массив букв, которые уже были
-        this.letterId; //id буквы, которая выбрана сейчас
         this.mainPlace = document.getElementById('main__place'); //разметка, куда рендерится основное поле
         this.countAllId = document.getElementById('countAll'); //разметка для рендера счёта и пропущеных букв
         this.countAnswer = countAnswer; //количество букв, на которые был ответ
@@ -20,8 +19,8 @@ class Alpabet {
         this.skipClass;
         this.skipCount;
         this.letterCount = letterCount;
-        this.renderAllLetters = false;
-        this.i;
+        this.i = i;
+        this.chooseLetter;
     }
 
     _init() {
@@ -33,7 +32,6 @@ class Alpabet {
         this.allData = await response.json();
 
         delete this.allData[0];
-
         this.start();
     }
 
@@ -42,8 +40,7 @@ class Alpabet {
         this.mainPlace.insertAdjacentHTML('beforeend', `
         <div class="button">
         <button class="button__start" id="btn_start">Начать</button>
-        </div>
-        `);
+        </div>`);
 
         document.getElementById('btn_start').addEventListener('click', () => {
             this.mainPlace.innerHTML = '';
@@ -52,7 +49,6 @@ class Alpabet {
         });
     }
     renderCount() {
-        console.log(this.countAnswer);
         ((this.skipLetter.length > 0) || (this.countAnswer > 0)) ? this.divClass = "count": this.divClass = "novisible";
         (this.skipLetter.length > 0) ? this.skipClass = "skip__letter": this.skipClass = "novisible";
         (this.countAnswer > 0) ? this.skipCount = "count__numbers": this.skipCount = "novisible";
@@ -76,37 +72,43 @@ class Alpabet {
 
         this.mainPlace.insertAdjacentHTML('beforeend', strLetter + strImg + strInput);
 
-        let chooseLetter;
+
         if (this.letterCount < 34) {
-            chooseLetter = this.allData[this.letterCount];
+            this.chooseLetter = this.allData[this.letterCount];
         } else if (this.letterCount > 33 && this.skipLetter.length > 0) {
-            chooseLetter = this.allData[this.skipLetter[this.i]];
+            this.chooseLetter = this.allData[this.skipLetter[this.i]];
+            if (this.i < this.skipLetter.length) { this.i++ }
         } else {
             this.finish();
         }
-        // (!this.renderAllLetters) ? chooseLetter = this.allData[this.letterCount]: chooseLetter = this.skipLetter[i];
 
-        //Рендер изображения
-        let letterCode = document.getElementById('letter_img');
-        letterCode.innerHTML = `<img src="img/${chooseLetter.small}" title="${chooseLetter.alt}"><p class="hint">${chooseLetter.hint}</p>`;
+        if (this.countAnswer < 33) {
 
-        let keyd = document.getElementById('letter_check');
-        keyd.addEventListener('keydown', function(e) {
-            if (e.keyCode === 13) {
-                e.preventDefault();
-                newLetter.showAnswer(keyd.value, chooseLetter.name, chooseLetter.id, chooseLetter.full);
-            }
-        });
+            // document.getElementById('letter_check').focus();
 
-        //Событие: нажали кнопку "Ответить"
-        document.getElementById('btnAnswer').addEventListener('click', () => {
-            this.showAnswer(keyd.value, chooseLetter.name, chooseLetter.id, chooseLetter.full);
-        });
 
-        //Слушаю событие: нажать на ссылку "Пропустить букву"
-        document.getElementById('skipLetter').addEventListener('click', () => {
-            this.skip_letter(chooseLetter.id);
-        });
+            //Рендер изображения
+            let letterCode = document.getElementById('letter_img');
+            letterCode.innerHTML = `<img src="img/${this.chooseLetter.small}" title="${this.chooseLetter.alt}"><p class="hint">${this.chooseLetter.hint}</p>`;
+
+            let keyd = document.getElementById('letter_check');
+            keyd.addEventListener('keydown', function(e) {
+                if (e.keyCode === 13) {
+                    e.preventDefault();
+                    newLetter.showAnswer(keyd.value, newLetter.chooseLetter.name, newLetter.chooseLetter.id, newLetter.chooseLetter.full);
+                }
+            });
+
+            //Событие: нажали кнопку "Ответить"
+            document.getElementById('btnAnswer').addEventListener('click', () => {
+                this.showAnswer(keyd.value, this.chooseLetter.name, this.chooseLetter.id, this.chooseLetter.full);
+            });
+
+            //Слушаю событие: нажать на ссылку "Пропустить букву"
+            document.getElementById('skipLetter').addEventListener('click', () => {
+                this.skip_letter(this.chooseLetter.id);
+            });
+        }
     }
 
     showAnswer(answer, name, id, full) {
@@ -129,19 +131,15 @@ class Alpabet {
         this.countAnswer++;
         this.letterCount++;
 
-        if (this.i < this.skipLetter.length) {
-            this.i++;
-        }
-
         this.renderCount();
         this.check_answer();
+
+        if (this.countAnswer == 33) { this.finish() };
 
         document.getElementById('btn_start').addEventListener('click', () => {
             this.mainPlace.innerHTML = '';
             this.renderMain();
         });
-
-        if (this.countAnswer == 33) { this.finish() };
     }
 
     check_answer() {
@@ -150,12 +148,13 @@ class Alpabet {
 
     check_skip() {
         let skipstr;
-        if (this.skipLetter.length == 1) {
+        let skipRight = this.skipLetter.length - this.i;
+        if (skipRight == 0) { skipstr = ""; } else if (skipRight == 1) {
             skipstr = `Пропущена 1 буква`;
-        } else if ((this.skipLetter.length >= 2) && (this.skipLetter.length <= 4)) {
-            skipstr = `Пропущено ${this.skipLetter.length} буквы`;
+        } else if ((skipRight >= 2) && (skipRight <= 4)) {
+            skipstr = `Пропущено ${skipRight} буквы`;
         } else {
-            skipstr = `Пропущено ${this.skipLetter.length} букв`;
+            skipstr = `Пропущено ${skipRight} букв`;
         }
 
         document.getElementById('countSkip').insertAdjacentHTML('beforeend', skipstr);
@@ -170,8 +169,49 @@ class Alpabet {
     }
 
     finish() {
-        console.log('Всё');
-        this.letterArr = []
+        this.countAllId.innerHTML = '';
+        this.mainPlace.innerHTML = '';
+
+        let strCount;
+        let strImg;
+        let strText;
+        let strBtn;
+
+        strCount = `<div class="result">
+        <p class="result__count">ТВОЙ РЕЗУЛЬТАТ</p>
+        <p class="result__numbers">Верных ответов: ${this.rightAnswer} из ${this.countAnswer}.</p>`;
+
+        strImg = `<div class="result__img">
+        <div class="result__cloud">Поздравляем! Отличный результат. <br>Если бы мы с тобой придумывали азбуку, то сделали бы это быстрее и лучше.</div>
+        </div>`;
+
+        strText = `<div class="result__text">
+        <p class="result__text_item">Надеюсь, что игра тебе понравилась. Сейчас в игре на каждую букву только один вариант, поэтому повторно её проходить уже не так интересно.</p>
+        <p class="result__text_item">Скоро добавлю ещё несколько вариантов.</p>
+        <p class="result__text_item">А сейчас расскажи об этой игре своим друщьям и знакомым.</p>
+        <p class="result__text_item">Кроме этого, ты можешь поблагодарить разработчика или посмотреть другие проекты.</p></div>`;
+
+        strBtn = `<div class="button">
+        <button class="button__start" id="btn_start">Повторить</button>
+        </div>
+        </div>`;
+
+        this.mainPlace.insertAdjacentHTML('beforeend', strCount + strImg + strText + strBtn);
+        document.getElementById('btn_start').addEventListener('click', () => {
+            this.mainPlace.innerHTML = '';
+            this.restart();
+        });
+    }
+
+    restart() {
+        this.allData = [];
+        this.letterArr = [];
+        this.countAnswer = 0;
+        this.rightAnswer = 0;
+        this.skipLetter = [];
+        this.letterCount = 1;
+        this.i = 0;
+        this.getLetters();
     }
 }
 
